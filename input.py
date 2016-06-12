2859cba8d7b8f917ef01cbb8d2e471146b2a73c1
cat40 <johnthecat492@gmail.com>
2016-06-11 20:48:12 -0400

    Fixed input bug
    Fixed crash when using cat.input.getnun() with invalid input

--- input.py
+++ input.py
@@ -1,3 +1,4 @@
+import cat
 '''
 Gets a number from user input. Allows expressions.
 Allows retries until valid input achived.
@@ -6,7 +7,7 @@ def getnum(string=''):
     while True:
         try: n = eval(raw_input(string))
         except (SyntaxError, NameError, ZeroDivisionError), e:
-            error.error(str(e))
+            cat.error.error(str(e))
             print('please try again')
             pass
         else: break

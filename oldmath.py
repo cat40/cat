f8a19fb95fa3f91bb2340329d1f75ae4470a9d97
cat40 <johnthecat492@gmail.com>
2016-06-13 21:41:27 -0400

    Added isfrac()

--- oldmath.py
+++ oldmath.py
@@ -32,3 +32,14 @@ def closeint(num, r=.05):
     n = round(num, p)
     i = int(round(num, 0))
     return n <= i+r and num >= i-r
+
+'''
+compares to list of known fractions
+returns true if in it
+'''
+def isfrac(num, fracs = [1./16, 2./16, 3./16, 4./16, 5./16, 6./16, 7./16, 8./16,
+                         9./16, 10./16, 11./16, 12./16, 13./16, 14./16, 15./16,
+                         1./9, 2./9, 3./9, 4./9, 5./9, 6./9, 7./9, 8./9]):
+    for i, f in enumerate(fracs):
+        fracs[i] = round(f, decimalplaces(num))
+    return num in fracs
